e5533b7f4e2fb73381447d858565452ee90cb6bf
swakzn <byunm58@gmail.com>
2023-09-15 21:58:50 +0600

    dz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Squares extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            block1: 'red',
+            block2: 'blue'
+        };
+    }
+
+    toggleBlocks = () => {
+        this.setState((prevState) => ({
+            block1: prevState.block1 === 'red' ? 'blue' : 'red',
+            block2: prevState.block2 === 'blue' ? 'red' : 'blue'
+        }));
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div
+                    className="square"
+                    style={{ backgroundColor: this.state.block1 }}
+                    onClick={this.toggleBlocks}
+                ></div>
+                <div
+                    className="square"
+                    style={{ backgroundColor: this.state.block2 }}
+                    onClick={this.toggleBlocks}
+                ></div>
+            </div>
+        );
+    }
 }
 
-export default App;
+export default Squares;
